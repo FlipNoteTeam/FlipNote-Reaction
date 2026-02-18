071e20e3843ae0637f121894aa8fb243584f3447
dungbik <q1w2e30630@gmail.com>
2026-02-18 14:12:31 +0900

    Init Project

---
+++ src/test/java/flipnote/reaction/ReactionApplicationTests.java
@@ -0,0 +1,13 @@
+package flipnote.reaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
