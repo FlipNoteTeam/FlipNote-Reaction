1c8302774c970685cdbbf26b58a610db85399c57
dungbik <q1w2e30630@gmail.com>
2026-04-03 14:19:16 +0900

    Fix: RabbitMQ 설정 오류

--- src/main/java/flipnote/reaction/infrastructure/messaging/RabbitMqConfig.java
+++ src/main/java/flipnote/reaction/infrastructure/messaging/RabbitMqConfig.java
@@ -1,7 +1,7 @@
 package flipnote.reaction.infrastructure.messaging;
 
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +23,6 @@ public class RabbitMqConfig {
 
 	@Bean
 	public MessageConverter jackson2JsonMessageConverter() {
-		return new Jackson2JsonMessageConverter();
+		return new JacksonJsonMessageConverter();
 	}
 }
